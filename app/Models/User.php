a1ef654dbfb5363ae4df913f6e154d32df0bcf76
isheun <jasonamanda1@gmail.com>
2025-07-31 21:38:30 +0700

    fix error di model user

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,6 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Auth;
 use Spatie\Activitylog\LogOptions;
 use Spatie\Activitylog\Traits\LogsActivity;
-use Illuminate\Support\Facades\Auth;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
